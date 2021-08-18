85fffd74ac7afb068ef2ded3e54fcaabebd2dc5b
longmen06 <longmenjailbreak@gmail.com>
2021-08-18 23:18:57 +0700

    uprepo

--- Loading.sh
+++ Loading.sh
@@ -55,7 +55,7 @@ apt-ftparchive packages ./DEB > ./Packages;
 bzip2 -c9k ./Packages > ./Packages.bz2;
 echo "------------------"
 echo "Đang tạo Release...."
-printf "Origin: Long Mến❤️\nLabel: Long Mến❤️\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm\nComponents: main\nDescription: Repo by Long Mến❤️\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
+printf "Origin: MOMOLAND\nLabel: Momoland❤️\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm\nComponents: main\nDescription: BBoom BBoom❤️\nMD5Sum:\n "$(cat ./Packages | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages --printf="%s")" Packages\n "$(cat ./Packages.bz2 | md5sum | cut -d ' ' -f 1)" "$(stat ./Packages.bz2 --printf="%s")" Packages.bz2\n" >Release;
 
 echo "------------------"
 echo "Xong !"
